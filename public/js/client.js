ddc2c080c9957456398f825ed85d57b2ece1a123
maliaoreta <maliaoreta@gmail.com>
2016-04-04 14:45:06 -1000

    needs fixing, tries to save search history

--- public/js/client.js
+++ public/js/client.js
@@ -5,12 +5,14 @@ var submitButton = document.getElementById('submitButton');
 
     var result = new XMLHttpRequest();
     result.addEventListener('load', resultListener);
+
     var queryTerm = document.getElementById('inputField').value;
+
     result.open('GET', 'https://api.gettyimages.com/v3/search/images?phrase=' + queryTerm);
     result.setRequestHeader('Api-Key', API_KEY);
     result.send();
-  });
 
+  });
 
 function resultListener () {
 
@@ -20,12 +22,34 @@ function resultListener () {
   if (resultData.result_count >= 4) {
 
     var posted = 0;
-    while (posted <= 4) {
-    var image = document.createElement('img');
-      image.id = 'image';
-      image.src = resultData.images[posted].display_sizes[0].uri; 
-      contentContainer.appendChild(image);
+    while (resultData.images[posted] !== undefined) {
+    
+      var imageDivs = document.createElement('div');
+        imageDivs.className = 'imageDivs';
+        contentContainer.appendChild(imageDivs);
+
+      var image = document.createElement('img');
+        image.id = 'image';
+        image.src = resultData.images[posted].display_sizes[0].uri; 
+        imageDivs.appendChild(image);
+
+      var imgCap = document.createElement('div');
+        imgCap.id = 'imgCap';
+        imgCap.innerHTML = resultData.images[posted].title;
+        imageDivs.appendChild(imgCap);
+
       posted++;
     }
   } 
 }
+
+// NEEDS FIXING
+var inputField = document.getElementById('inputField');
+  inputField.addEventListener('change', function ( ){
+
+    sessionStorage.setItem('searchHistory', inputField.value);
+    console.log(sessionStorage.getItem('searchHistory'));
+
+  });
+
+  console.log(sessionStorage);
